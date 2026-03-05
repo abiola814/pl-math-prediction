0fac7cfa63fb6d43048b39f03ac1dd53bcef6d66
abiola814 <oladunjoye1998@gmail.com>
2026-03-05 03:36:35 +0000

    fix:allo origin

--- pl-math-engine/backend/app/main.py
+++ pl-math-engine/backend/app/main.py
@@ -26,7 +26,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://prempredict.3gensolution.co.uk",
+        "https://www.prempredict.3gensolution.co.uk",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
